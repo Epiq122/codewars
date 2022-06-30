3788c1110b538f173f415565cf38fdf35e144546
Epiq122 <l2ob122@gmail.com>
2022-06-29 18:26:14 -0700

    finished jun 29 2022

--- 6kyu/pangram.js
+++ 6kyu/pangram.js
@@ -1,4 +1,4 @@
-// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
 
 function isPangram(string) {
   return (
---
+++ 7kyu/everysquare.js
@@ -0,0 +1,9 @@
+function squareDigits(num) {
+  return parseInt(
+    num
+      .toString()
+      .split('')
+      .map((x) => Math.pow(x, 2))
+      .join('')
+  );
+}
