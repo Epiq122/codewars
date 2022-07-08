08533ec9d3d538a358cea380c616d8f20f795371
Epiq122 <l2ob122@gmail.com>
2022-07-08 16:08:26 -0700

    finished on july 8 2022

--- 6kyu/tribonac.js
+++ 6kyu/tribonac.js
@@ -1,11 +1,11 @@
 function tribonacci(signature, n) {
-  if (n === 0) return [];
-  if (n === 1) return [signature[0]];
-  if (n === 2) return [signature[0], signature[1]];
-  if (n === 3) return [signature[0], signature[1], signature[2]];
-  let result = [signature[0], signature[1], signature[2]];
-  for (let i = 3; i < n; i++) {
-    result.push(result[i - 1] + result[i - 2] + result[i - 3]);
-  }
-  return result;
+    if (n === 0) return [];
+    if (n === 1) return [signature[0]];
+    if (n === 2) return [signature[0], signature[1]];
+    if (n === 3) return [signature[0], signature[1], signature[2]];
+    let result = [signature[0], signature[1], signature[2]];
+    for (let i = 3; i < n; i++) {
+        result.push(result[i - 1] + result[i - 2] + result[i - 3]);
+    }
+    return result;
 }
