15e963cae6b65a0033b1e2a1963f5f61b8fb828a
Epiq122 <l2ob122@gmail.com>
2022-07-10 17:59:26 -0700

    finished on july 10 2022

--- 6kyu/alpha.js
+++ 6kyu/alpha.js
@@ -1,10 +1,5 @@
 function alphabetPosition(text) {
-  return text
-    .toLowerCase()
-    .replace(/[^a-z]/g, "")
-    .split("")
-    .map(function (char) {
-      return char.charCodeAt(0) - 96;
-    })
-    .join(" ");
+  return text.toLowerCase().replace(/[^a-z]/g, '').split('').map(function(char) {
+    return char.charCodeAt(0) - 96;
+  }).join(' ');
 }
